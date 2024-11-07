3a542c101661b9d8d8baa2f65ad8f767da31f39f
recrutor25 <sommersi@yandex.ru>
2024-11-07 10:57:54 +0200

    06.11.24

--- module_5_1.py
+++ module_5_1.py
@@ -10,16 +10,16 @@ class House:
  # Создаю метод go_to с параметром new_floor и логикой внутри
     def go_to(self, new_floor: int):
 
-        if 1 < new_floor > self.num_floors:
-            print('Такого этажа не существует!')
-        else:
+        if 1 <= new_floor < self.num_floors:
             for i in range(1, new_floor + 1):
                 print(i)
+        else:
+            print('Такого этажа не существует!')
 
 # Создаю объект класса House с названием и количеством этажей
 h1 = House('ЖК Горский', 18)
 h2 = House('Домик в деревне', 2)
 
 # Вывод на консоль:
-h1.go_to(1)
+h1.go_to(5)
 h2.go_to(10)
